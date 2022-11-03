6ea1522645da3f95154da5f19abd6ab12e20d780
feriafrianto <feriafrianto7@gmail.com>
2022-11-03 22:59:27 +0700

    init project

---
+++ app/Http/Controllers/TouristAttractionController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TouristAttraction;
+use Illuminate\Http\Request;
+
+class TouristAttractionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\TouristAttraction  $touristAttraction
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TouristAttraction $touristAttraction)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\TouristAttraction  $touristAttraction
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(TouristAttraction $touristAttraction)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\TouristAttraction  $touristAttraction
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TouristAttraction $touristAttraction)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\TouristAttraction  $touristAttraction
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TouristAttraction $touristAttraction)
+    {
+        //
+    }
+}
